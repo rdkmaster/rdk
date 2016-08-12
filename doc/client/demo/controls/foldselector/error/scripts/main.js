dc3abcfc5aeab40bf9a40b754bc431d584b90edb
顾姗6092001913 <gushan0526@126.com>
2016-08-12 11:26:06 +0800

    update folderselector demo

--- doc/client/demo/controls/foldselector/error/scripts/main.js
+++ doc/client/demo/controls/foldselector/error/scripts/main.js
@@ -1,10 +1,9 @@
-define('main', ['rd.controls.FoldSelector','rd.controls.BasicSelector','rd.containers.Accordion'], function() {
+define('main', ['rd.controls.FoldSelector'], function() {
 
     // 创建一个RDK的应用
-    var app = angular.module("rdk_app", ['rd.controls.FoldSelector','rd.controls.BasicSelector','rd.containers.Accordion']);
+    var app = angular.module("rdk_app", ['rd.controls.FoldSelector']);
     // 创建一个控制器
-    app.controller('myCtrl', ['$scope', 'EventService', 'EventTypes', 'Utils', '$timeout',
-        function(scope, EventService, EventTypes, Utils, $timeout) {
+    app.controller('myCtrl', ['$scope', function(scope) {
 
             scope.allItems = [
                 { id: 0, label: "江苏省" },
@@ -18,21 +17,21 @@ define('main', ['rd.controls.FoldSelector','rd.controls.BasicSelector','rd.conta
                 { id: 8, label: "新疆省" },
                 { id: 9, label: "四川省" },
             ];
-        
+
             scope.dimSelectedItems = [
                 { id: 3, label: "广西省" },
                 { id: 7, label: "湖南省" },
                 { id: 4, label: "河北省" },
             ];
-                                               
+
             scope.raiseErrorAct = function(event, info) {
                 var errorCode = info.code;
                 var errorMsg = '';
-                if(errorCode == 101) {
+                if (errorCode == 101) {
                     errorMsg = '【自定义error】选择条目不可为空！';
-                } else if(errorCode == 102){
+                } else if (errorCode == 102) {
                     errorMsg = '【自定义error】至少选择2条信息！';
-                } else{
+                } else {
                     errorMsg = '【自定义error】其它异常！';
                 }
                 console.log('异常码:' + info.code + ';\n自定义异常信息：' + errorMsg + ';\n控件返回异常信息：' + info.message);
--- doc/client/demo/controls/foldselector/selected_items/scripts/main.js
+++ doc/client/demo/controls/foldselector/selected_items/scripts/main.js
@@ -1,33 +1,33 @@
-define('main', ['rd.controls.BasicSelector', 'rd.containers.Accordion', 'rd.controls.FoldSelector'], function(){
-	var app = angular.module('rdk_app', ['rd.controls.BasicSelector','rd.containers.Accordion', 'rd.controls.FoldSelector']);
-	app.controller('foldSelectorCtrl', ['$scope', '$timeout', 'EventService', 'EventTypes', function (scope, $timeout, EventService, EventTypes) {
-		scope.data = [
-			{ id: 0, label: "江苏省" },
-			{ id: 1, label: "浙江省" },
-			{ id: 2, label: "广东省" },
-			{ id: 3, label: "广西省" },
-			{ id: 4, label: "河北省" },
-			{ id: 5, label: "河南省" },
-			{ id: 6, label: "湖北省" },
-			{ id: 7, label: "湖南省" },
-			{ id: 8, label: "新疆省" },
-			{ id: 9, label: "四川省" },
-		];
+define('main', ['rd.controls.FoldSelector'], function() {
+    var app = angular.module('rdk_app', ['rd.controls.FoldSelector']);
+    app.controller('foldSelectorCtrl', ['$scope', '$timeout', function(scope, $timeout) {
+        scope.data = [
+            { id: 0, label: "江苏省" },
+            { id: 1, label: "浙江省" },
+            { id: 2, label: "广东省" },
+            { id: 3, label: "广西省" },
+            { id: 4, label: "河北省" },
+            { id: 5, label: "河南省" },
+            { id: 6, label: "湖北省" },
+            { id: 7, label: "湖南省" },
+            { id: 8, label: "新疆省" },
+            { id: 9, label: "四川省" },
+        ];
 
-	    scope.selectedItems = [
-	        { id: 3, label: "广西省" },
-	        { id: 7, label: "湖南省" },
-	        { id: 4, label: "河北省" },
-	    ];
+        scope.selectedItems = [
+            { id: 3, label: "广西省" },
+            { id: 7, label: "湖南省" },
+            { id: 4, label: "河北省" },
+        ];
 
-	    scope.changeHandler = function(event, data){
-	        console.log(scope.selectedItems);//里面影响外面
-	    }
+        scope.changeHandler = function(event, data) {
+            console.log(scope.selectedItems); //里面影响外面
+        }
 
-	    $timeout(function(){
-	    	scope.selectedItems = [{id: 3, label: "广西省"}];//外面影响里面
-	    }, 3000);
+        $timeout(function() {
+            scope.selectedItems = [{ id: 3, label: "广西省" }]; //外面影响里面
+        }, 3000);
 
-	}])
+    }])
 
 });
