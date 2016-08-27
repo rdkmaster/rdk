a5fb2a4498b369304e4f543874cf98e221638132
何建洋6270000482 <18651436384@163.com>
2016-08-27 15:17:40 +0800

    test_helper文件的改动保存

--- test/module/testee_helper.js
+++ test/module/testee_helper.js
@@ -1,6 +1,12 @@
 define(['rd.core'], function() {
 
     function commonAjaxConfigProcessor(config) {
+        if (!_isService(config.url)) {
+            //请求的不是RDK的REST服务
+            return config;
+        }
+
+        //请求的是RDK的REST服务
         var url = _fixUrl(config.url).trim();
         config.url = '/rdk/service/app/common/relay';
         var key = config.method == 'get' ? 'params' : 'data';
@@ -18,19 +24,26 @@ define(['rd.core'], function() {
     }
 
     function commonDataProcessor(data, dataSource) {
-        try {
-            return JSON.parse(data.result);
-        } catch (e) {
-            return data.result;
+        var result;
+        if (data.hasOwnProperty('result') && angular.isString(data.result)) {
+            try {
+                result = JSON.parse(data.result);
+            } catch (e) {
+                result = data.result;
+            }
+        } else {
+            result = data;
         }
+        return result;
+    }
+
+    function _isService(url) {
+        return url.search(/\$svr/) != -1;
     }
 
     function _fixUrl(url) {
-        if (url.search(/\$svr/) != -1) {
-            var svr = location.href.match(/\/test\/(.*)\/web\//)[1];
-            url = url.replace(/\$svr/, '../test/' + svr + '/server');
-        }
-        return url;
+        var svr = location.href.match(/\/test\/(.*)\/web\//)[1];
+        return url.replace(/\$svr/, '../test/' + svr + '/server');
     }
 
     return {
