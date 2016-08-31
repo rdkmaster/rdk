7a3adab079d189b5ef126c8d30ce8cf8bd30a48c
陈旭145812 <chen.xu8@zte.com.cn>
2016-08-31 14:46:55 +0800

    修复linux启动脚本

--- start.sh
+++ start.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
 #For run the rdk_server on linux machine
-################################################################
-#start rdk_server,and the service rest & http based on nodejs### 
-#nodes:make sure the linux machine has installed nodejs!!!#####
-################################################################
+##################################################################
+#  start rdk_server,and the service rest & http based on nodejs  # 
+#    nodes:make sure the linux machine has installed nodejs!!!   #
+##################################################################
 
 cur_dir=$(cd "$(dirname "$0")"; pwd) 
 
-START() {
-    ##rdk_server
-	sed -i 's/localhost/10\.9\.233\.35/g' $cur_dir/rdk/proc/conf/rdk.cfg
-    cd $cur_dir/rdk/proc/bin/
-    sh run.sh > /dev/null
 
-    node="$cur_dir/tools/node-linux-x64/bin/node"
-    chmod +x $node
-	
-    ##http
-	sed -i 's/localhost/10\.9\.233\.35/g' $cur_dir/tools/http_server/config.json
-    cd $cur_dir/tools/http_server
-    nohup $node server.js & > /dev/null
+node="$cur_dir/tools/node-linux-x64/bin/node"
+if [ ! -e $node ]; then 
+	node="node"
+fi
+chmod +x $node
+$node --version > /dev/null
+if [ ! $? -eq 0 ]; then
+	echo "ERROR: need NodeJs runtime enviroment!"
+	exit 1
+fi
 
-    ##http
-	cd $cur_dir/doc/tools/live_demo/mock_rest
-    nohup $node rest_service.js & > /dev/null
-}
 
-START
+## start http server
+cd $cur_dir/tools/http_server
+nohup $node server.js &
+
+## start rest server
+cd $cur_dir/doc/tools/live_demo/mock_rest
+nohup $node rest_service.js &
+
+## start rdk server
+cd $cur_dir/rdk/proc/bin/
+sh run.sh
 
