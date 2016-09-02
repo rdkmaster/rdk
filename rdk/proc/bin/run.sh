8b2194d47901296728d6c3aca5f7b0f9d4d0daa5
刘伟10184092 <liu.wei21@zte.com.cn>
2016-09-02 13:35:30 +0800

    update

--- rdk/proc/bin/run.sh
+++ rdk/proc/bin/run.sh
@@ -6,8 +6,8 @@ if [ "" != "$pid" ]; then
 	echo "RDK Server (pid=$pid) has been already running..."
 	exit
 fi
-#jvm_opts="$jvm_opts -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n"
-jvm_opts="-server -Xms4g -Xmx4g -XX:-UseGCOverheadLimit"
+jvm_opts="$jvm_opts -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n"
+jvm_opts="-server -Xms4g -Xmx4g -XX:-UseGCOverheadLimit $jvm_opts"
 
 JAVA_HOME="./proc/bin/jre"
 
@@ -18,7 +18,7 @@ chmod 777 $JAVA_HOME/bin/*
 DIRNAME=`dirname $0`
 program_root=`cd "$DIRNAME"/; pwd`
 
-classPath="$program_root/lib/*"
+classPath="$program_root/proc/bin/lib/*"
 
 echo "RDK Server is starting..."
 $JAVA_HOME/bin/java $jvm_opts -Dfile.encoding=UTF-8 -D$rdk_flag -classpath "$classPath" com.zte.vmax.rdk.Run &
