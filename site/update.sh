802a1ad088d48df1effd0c7cd262b0e92ef3f7f1
陈旭145812 <chen.xu8@zte.com.cn>
2016-09-05 14:50:34 +0800

    去掉update.sh中修改5812和8080端口的功能

--- site/update.sh
+++ site/update.sh
@@ -22,12 +22,11 @@ sh ./shutdown.sh > /dev/null
 
 cd $basepath/..
 # 换一个新的RDK进程监听端口，避免与已有端口冲突
-sed -i 's/5812/5813/g' rdk/proc/conf/rdk.cfg
-sed -i 's/5812/5813/g' tools/http_server/config.json
+#sed -i 's/5812/5813/g' rdk/proc/conf/rdk.cfg
+#sed -i 's/5812/5813/g' tools/http_server/config.json
+
 # web服务器端口改为默认80端口
-sed -i 's/8080/80/g' tools/http_server/config.json
-#sed -i 's/localhost/10\.43\.149\.163/g' rdk/proc/conf/rdk.cfg
-#sed -i 's/localhost/10\.43\.149\.163/g' tools/http_server/config.json
+#sed -i 's/8080/80/g' tools/http_server/config.json
 
 sh start.sh > /dev/null
 
