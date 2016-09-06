fb85bca50aba140f131c39e2dcca14908e2cc8c6
张人杰6176000040 <zhang.renjie@zte.com.cn>
2016-09-06 14:36:23 +0800

    test

---
+++ doc/client/demo/comprehensive/scoreIndicator/web/scripts/main.js
@@ -0,0 +1,53 @@
+define('main', ['application','rd.services.EventService', 'rd.controls.ScoreIndicator','rd.services.Utils'], function(application, EventService,Utils) {
+
+// 创建一个RDK的应用
+var app = angular.module("rdk_app", ['rd.controls.ScoreIndicator','rd.services.Utils']);
+// 创建一个控制器
+app.controller('myCtrl', ['$scope', '$timeout','DataSourceService', 'EventService','Utils',  function(scope, $timeout,DataSourceService, EventService,Utils) {
+	application.initDataSourceService(DataSourceService);
+/******************************************************
+     将应用的代码逻辑添加在这个匿名函数内部
+******************************************************/
+	// document.body.addEventListener('app_loaded',function(){
+	// 	console.log($('.hrLinePart1')[0]);
+	// });
+	EventService.register('my_ds', 'result', function(event, data) {
+		scope.isMark = false;
+		scope.config = [{
+		    label: '优',
+		    color: '#55ca8c',
+		    emotion: '/doc/client/demo/controls/scoreindicator/faceImage/face1.png',
+		    value: data.data[0],
+		    mark: false
+		}, {
+		    label: '良',
+		    color: '#54acd5',
+		    emotion: '/doc/client/demo/controls/scoreindicator/faceImage/face2.png',
+		    value: data.data[1],
+		    mark: true
+		}, {
+		    label: '中',
+		    color: '#f99660',
+		    emotion: '/doc/client/demo/controls/scoreindicator/faceImage/face3.png',
+		    value: data.data[2],
+		    mark: false
+		}, {
+		    label: '差',
+		    color: '#ec6d6d',
+		    emotion: '/doc/client/demo/controls/scoreindicator/faceImage/face4.png',
+		    value: data.data[3],
+		    mark: false
+		}];
+	});
+	// EventService.register('EventService', 'ready', function() {
+	//     console.log($('.hrLinePart1')[0]);
+	// });
+	// Utils.onReady = function(EventService) {
+	// 	console.log($('.hrLinePart1')[0]);
+	// 	// EventService.ready=function(){
+	// 	// 	console.log($('.hrLinePart1')[0]);
+	// 	// }
+	//  }
+}
+]);
+});
---
+++ doc/client/demo/comprehensive/tab/simple1/web/scripts/main.js
@@ -0,0 +1,14 @@
+define('main', ['rd.containers.Tab'], function() {
+
+    // 创建一个RDK的应用
+    var app = angular.module("rdk_app", ['rd.containers.Tab']);
+    // 创建一个控制器
+    app.controller('myCtrl', ['$scope', function(scope) {
+            /******************************************************
+                 将应用的代码逻辑添加在这个匿名函数内部
+            ******************************************************/
+            //no code here
+        }
+
+    ]);
+});
