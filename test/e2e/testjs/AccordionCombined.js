8aed308fe4bf99b82447cb9d6bc76f96c030d1cb
何建洋6270000482 <18651436384@163.com>
2016-09-24 13:24:55 +0800

    修改index文件保证截图路径准确

---
+++ test/e2e/testjs/AccordionCombined.js
@@ -0,0 +1,12 @@
+'use strict';
+describe(function(){
+    beforeEach(function(){
+        browser.get('test/e2e/testee/accordion/web/combined.html');
+        browser.sleep(2000);
+    });
+    afterEach();
+    it('展开accordion并且能够点击combo下面的basic_select控件',function(){
+        
+    });
+
+});
