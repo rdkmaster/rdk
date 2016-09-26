8f452e1b0a6f0f31fd0961b223424c5389877fb8
陈运生6396000843 <chen.yunsheng1@zte.com.cn>
2016-09-26 14:38:12 +0800

    把测试数据写入文件中

--- doc/client/demo/comprehensive/tab/accordion_tab/web/scripts/accordion.js
+++ doc/client/demo/comprehensive/tab/accordion_tab/web/scripts/accordion.js
@@ -20,80 +20,17 @@ define('main', ['application', 'blockUI','rd.controls.Table', 'rd.containers.Tab
                     scope.btnStatus=!scope.btnStatus;
                     //折叠收缩content的显示，TODO:控制器中不应该进行DOM操作。。
                     if(!scope.btnStatus){
-                        $(".tab_wrap").find(".rdk-tab-content").addClass("content-hiddan");
+                        $(".tab_wrap").find(".content").addClass("content-hiddan");
                         //setTimeout(function(){
                         //    $(".tab_wrap").find(".rdk-tab-content").hide();
                         //},1000);
-
                     }else{
-                        $(".tab_wrap").find(".rdk-tab-content").removeClass("content-hiddan");
+                        $(".tab_wrap").find(".content").removeClass("content-hiddan");
                         //setTimeout(function(){
                         //    $(".tab_wrap").find(".rdk-tab-content").show();
                         //},1000);
                     }
-                }
-                scope.accordionData = {};
-                scope.accordionData.header = ["一级原因", "原因名称1", "原因名称2", "原因名称3"];
-                scope.accordionData.field = ["name", "position", "salary", "start_date"];
-                scope.accordionData.data = [
-                    [
-                        "次数",
-                        "890",
-                        "890",
-                        "890"
-                    ],
-                    [
-                        "占比",
-                        "90%",
-                        "90%",
-                        "90%"
-                    ]
-                ];
-                scope.accordionData2 = {};
-                scope.accordionData2.header = ["指标", "无线接通率", "掉线率", "切换成功率", "重定向占比", "影响用户数"];
-                scope.accordionData2.field = ["name", "position", "salary", "start_date", "start_date", "start_date"];
-                scope.accordionData2.data = [
-                    [
-                        "23",
-                        "89%",
-                        "3%",
-                        "86%",
-                        "86%",
-                        "58"
-                    ],
-                    [
-                        "23",
-                        "89%",
-                        "3%",
-                        "86%",
-                        "86%",
-                        "58"
-                    ],
-                    [
-                        "23",
-                        "89%",
-                        "3%",
-                        "86%",
-                        "86%",
-                        "58"
-                    ],
-                    [
-                        "23",
-                        "89%",
-                        "3%",
-                        "86%",
-                        "86%",
-                        "58"
-                    ],
-                    [
-                        "23",
-                        "89%",
-                        "3%",
-                        "86%",
-                        "86%",
-                        "58"
-                    ]
-                ];
+                };
                 /************************ accordion test data end ************************/
             }]);
     });
