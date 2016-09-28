89e416516957579928cff05b2dc694de31a6350a
刘伟10184092 <liu.wei21@zte.com.cn>
2016-09-28 08:52:18 +0800

    fix mq timeout

--- rdk/proc/utils/runtime_helper.js
+++ rdk/proc/utils/runtime_helper.js
@@ -45,7 +45,11 @@ var mq={
     p2p: function (subject, message) {
         return rdk_runtime.p2p(subject, message);
     },
+    //默认120秒
     rpc: function (subject, replySubject, message, timeout) {
+        if(!_.isDefined(timeout)){
+            timeout=120
+        }
         return rdk_runtime.rpc(subject, replySubject, message, timeout);
     },
     broadcast: function (subject, message) {
