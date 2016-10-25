64000d23180dd1c8407f7e4e2ef530936e992c4c
方亮6176000041 <fang.liang1@zte.com.cn>
2016-10-25 11:00:30 +0800

    解决单个icon和label单个存在居中问题（同时存在时有4px的间距）

--- rdk/app/libs/rdk/controls/Button.js
+++ rdk/app/libs/rdk/controls/Button.js
@@ -9,10 +9,10 @@ define(['rd.core', 'css!rd.styles.Button','css!rd.styles.FontAwesome'
                 transclude: true,
                 template:'<div class="rdk-button-wrap">\
                                 <div  class="rdk-button-comment" ng-click="setSelected()">\
-                                    <button class="rdk-button-btn " ng-click="click()" ng-mouseover="$mouseOver()" ng-mouseout="$mouseOut()"\
+                                    <button class="rdk-button-btn {{Fang}}" ng-mouseover="$mouseOver()" ng-mouseout="$mouseOut()"\
                                     title="{{tooltip}}" ng-class="{rdk_button_selected:toggle?selected:false,\
                                         rdk_button_enabled:!enabled,rdk_button_over: mouse}" ng-disabled="!enabled">\
-                                        <img src="{{icon}}" ng-click="$stopPro($event)" ng-show="iconShow" ><i class="{{icon}}" ng-show="!iconShow"></i>{{label}}\
+                                        <img src="{{icon}}" ng-click="$stopPro($event)" ng-if="iconShow" ng-class="{rdk_padding_right:paddingHide}"><i ng-click="$stopPro($event)" ng-class="{rdk_padding_right:paddingHide}" class="{{icon}}" ng-if="!iconShow"></i>{{label}}\
                                     </button>\
                                 </div>\
                            </div>',
@@ -23,11 +23,9 @@ define(['rd.core', 'css!rd.styles.Button','css!rd.styles.FontAwesome'
                     selected:'=?',
                     enabled:'=?',
                     toggle:'=?',
-                    tooltip:'@?'
+                    tooltip:'@?',
+                    type:'@?'
                 },
-                 controller: ['$scope', function(scope) {
-
-                }],
                 link: function(scope,ele, attr) {
                     scope.label = Utils.getValue(scope.label, attr.label, '');
                     scope.icon = Utils.getValue(scope.icon, attr.icon, false);
@@ -36,6 +34,7 @@ define(['rd.core', 'css!rd.styles.Button','css!rd.styles.FontAwesome'
                         if(scope.toggle==true){
                             scope.selected=!scope.selected
                        }
+                       scope.click();
                     }
                     scope.$stopPro=function($event){
                         if(!scope.enabled){
@@ -43,10 +42,10 @@ define(['rd.core', 'css!rd.styles.Button','css!rd.styles.FontAwesome'
                         }
                     }
                     scope.iconShow=false;
-                   var classFlag=/\/+/;
-                   if(classFlag.test(scope.icon)){
+                    var classFlag=/\/+/;
+                    if(classFlag.test(scope.icon)){
                     scope.iconShow=true
-                   }
+                    }
                     /* hover函数*/
                     scope.mouse=false;
                     scope.$mouseOver=function(){
@@ -56,14 +55,18 @@ define(['rd.core', 'css!rd.styles.Button','css!rd.styles.FontAwesome'
                         scope.mouse=false
                     }
                     /* 禁用时样式 */
+                    console.log(scope.label)
                     var btn = ele[0].children[0].children[0];
                     var img = ele[0].children[0].children[0].children[0];
-                    var btnHeight = parseFloat($(btn).attr('height'));
-                    var imgWidth=parseFloat($(img).css('width'));
-                    var imgHeight=parseFloat($(img).css('height'));
-                    if(scope.icon=="false"){
-                        $(img).css('display','none');
+                    scope.paddingHide=true;
+                    // console.log(!!scope.label)
+                    if(!!scope.icon&&scope.icon!="false"&&!!scope.label){
+                        scope.paddingHide=true
+                    }else{
+                        scope.paddingHide=false
                     }
+
+                    console.log(!!scope.icon,!!scope.label)
                 }
             };
         }
