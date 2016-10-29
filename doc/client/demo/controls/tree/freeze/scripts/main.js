54d440f141856c5018580878311e5d8f9ffead26
黄海宁6270000461 <644808070@qq.com>
2016-10-29 14:09:18 +0800

    treeDome

--- doc/client/demo/controls/tree/freeze/scripts/main.js
+++
@@ -1,9 +0,0 @@
-define('main', ['rd.controls.Tree'], function() {
-    // 创建一个RDK的应用
-    var app = angular.module("rdk_app", ['rd.controls.Tree','rd.core']);
-    // 创建一个控制器
-    app.controller('myCtrl', ['$scope', 'EventService', 'EventTypes', function($scope, EventService, EventTypes) {
-    	
-
-    }]);
-});
