03b5fc7f9c3482c5cbc4589b676248bb7b7270e1
黄海宁6270000461 <644808070@qq.com>
2016-10-31 10:34:22 +0800

    treeDome2

--- doc/client/demo/controls/tree/unselect_on_blur/scripts/main.js
+++ doc/client/demo/controls/tree/unselect_on_blur/scripts/main.js
@@ -4,6 +4,7 @@ define('main', ['rd.controls.Tree'], function() {
     // 创建一个控制器
 app.controller('myCtrl', ['$scope', 'EventService', 'EventTypes', 'DataSourceService' ,function($scope,EventService, EventTypes,DataSourceService) {    	
 		$(document).ready(function(){
+            //你想让那个区域点击失去焦点，就在这个区域上注册click事件
 	        $('#testZtree').on("click", function() {
 	            var nodes = rdk.testZtree.tree.getSelectedNodes();
 	            if(nodes.length>0){
@@ -12,9 +13,11 @@ app.controller('myCtrl', ['$scope', 'EventService', 'EventTypes', 'DataSourceSer
 	            }
 	        });
     	});
+        //这个是节点的click事件，节点的事件要阻止冒泡！
     	$scope.clickFun=function(event, data){
     		event.stopPropagation();
     	}
+        //这是用来在失去焦点后的，清空之前的选中信息。
         $scope.unselectFun=function(event, data){
             console.log(data[0].label);
         }
