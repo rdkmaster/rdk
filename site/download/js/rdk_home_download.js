e2375d34e727dfbea1120ba7402e50f34a6089e9
黄海宁6270000461 <644808070@qq.com>
2016-11-16 09:12:31 +0800

    rdkDow

--- site/download/js/rdk_home_download.js
+++ site/download/js/rdk_home_download.js
@@ -3,11 +3,27 @@ $(document).ready(function() {
 	var iframe = $('#iframe');
 	var download_develop = $("#download_develop");
 	var download_run = $("#download_run");
-	var init = 'v2.3.2';
+	var init = $('#nov li:first-child span').text();
+	var hrefInit = $('#nov li:first-child span').attr("href");
+	var hrefText = hrefInit.slice(hrefInit.indexOf("/")+1);
+	var textNub = hrefText.slice(hrefText.indexOf("v")+1,hrefText.indexOf("/"));
+	$.ajax({
+    	url: hrefInit,
+    	type: "get",
+    	dataType: "text",
+    	success: function(data, textStatus){
+    		iframe.append(data);
+    	}
+    });
+    download_develop.text('下载' + textNub + '开发包');
+    download_run.text('下载' + textNub + '运行包');
+
+
+
 	allSpan.each(function(){
 		$(this).click(function(){
-			$(this).css('color', '#5A5A5A');
-			$(this).parent().siblings('li').find('span').css('color', '#08c');
+			$(this).css({'color':'#fff',"background-color":"#98bf21"});
+			$(this).parent().siblings('li').find('span').css({"color":"#08c","background-color":"#fff"});
 			var href = $(this).attr("href");
 		    $.ajax({
 		    	url: href,
@@ -17,11 +33,20 @@ $(document).ready(function() {
 		    		iframe.empty();
 		    		iframe.append(data);
 		    	}
-		    })
+		    });
+
 			download_develop.attr("src", href);
 			download_run.attr("src", href);
+			var hrefText = href.slice(href.indexOf("/")+1);
+			var textNub = hrefText.slice(hrefText.indexOf("v")+1,hrefText.indexOf("/"));
+			download_develop.text('下载' + textNub + '开发包');
+   			download_run.text('下载' + textNub + '运行包');
+
 		});
 	});
+
+
+
 	download_develop.click(function(){
 		var href = $(this).attr("src");
 		if(!href){
@@ -31,6 +56,8 @@ $(document).ready(function() {
 		var url = "version/rdk-develop-environment" + edition.slice(edition.indexOf("v")+1,edition.indexOf("/")) + ".zip";
 		window.open(url,"_blank");
 	}) ;
+
+
 	download_run.click(function(){
 		var href = $(this).attr("src");
 		if(!href){
