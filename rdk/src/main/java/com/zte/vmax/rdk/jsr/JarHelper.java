2dc88ad2e6bb2d00db379b9a27f9094441301776
钱瑞伟10054860 <10054860@zte.intra>
2016-11-22 17:37:41 +0800

    commit code

--- rdk/src/main/java/com/zte/vmax/rdk/jsr/JarHelper.java
+++ rdk/src/main/java/com/zte/vmax/rdk/jsr/JarHelper.java
@@ -1,19 +1,15 @@
 package com.zte.vmax.rdk.jsr;
 
+import com.zte.vmax.rdk.loader.RdkClassLoader;
 import com.zte.vmax.rdk.log.AbstractAppLoggable;
 import com.zte.vmax.rdk.log.AppLogger;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.lang.Exception;
-import java.util.ArrayList;
 
 /**
  * Created by 00139520 on 16-4-13.
- * 
  */
 public class JarHelper extends AbstractAppLoggable {
 
@@ -21,62 +17,54 @@ public class JarHelper extends AbstractAppLoggable {
         logger = AppLogger.getLogger("JarHelper", appName);
     }
 
-    private URLClassLoader loadJars(String jar) {
-		jar = FileHelper.fixPath(jar, appName);
+
+    private void loadJars(String jar) {
+        jar = FileHelper.fixPath(jar, appName);
 
         File file = new File(jar);
         if (!file.exists()) {
             logger.error(jar + ",this directory does not exist.");
-            return null;
+            return;
         }
 
         File[] files;
         if (file.isDirectory()) {
             files = file.listFiles();
             if (files == null) {
-                return null;
+                return;
             }
         } else {
             files = new File[1];
             files[0] = file;
         }
-        ArrayList<URL> urls = new ArrayList<>();
+
         for (File f : files) {
             if (f.isFile() && f.getAbsolutePath().toLowerCase().endsWith(".jar")) {
                 try {
                     logger.debug("loading: " + f);
-                    urls.add(f.toURI().toURL());
+                    RdkClassLoader.addURL(f.toURI().toURL());
                 } catch (MalformedURLException e) {
                     logger.error(e);
                 }
             }
         }
 
-        URL[] aUrls = new URL[urls.size()];
-        urls.toArray(aUrls);
-        return new URLClassLoader(aUrls, Thread.currentThread().getContextClassLoader());
     }
 
     public Class<?> loadClass(String jar, String className) {
-        URLClassLoader urlLoader = loadJars(jar);
-        if (urlLoader == null) {
-            logger.error("invalid jar or jar path: " + jar);
-            return null;
-        }
-
-        Class<?> clazz = null;
+        loadJars(jar);
         try {
-            clazz = urlLoader.loadClass(className);
+           return RdkClassLoader.loadClass(className);
         } catch (Exception e) {
             logger.error("loadClass error: ", e);
             return null;
+        }finally {
+            try {
+                RdkClassLoader.close();
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+            }
         }
-        try {
-            urlLoader.close();
-        } catch (IOException e) {
-            logger.warn("close urlLoader error: ", e);
-        }
-        return clazz;
     }
 
 
