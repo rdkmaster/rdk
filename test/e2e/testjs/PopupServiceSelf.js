9042fef5bcf51db8cbb13dfa3d20bbb628c4aeb2
徐荔00111190 <xu.li9@zte.com.cn>
2016-11-23 15:10:14 +0800

    sleep 2000

--- test/e2e/testjs/PopupServiceSelf.js
+++ test/e2e/testjs/PopupServiceSelf.js
@@ -8,19 +8,19 @@ describe("PopupService Self Test",function(){
     it('弹出并单击关闭', function(){
         var btn = element.all(by.css(".demo button"));
         btn.get(0).click();
-        browser.sleep(300);
-        expect(element.all(by.css(".ui-dialog-content .sample-module")).count()).toBe(1);
+        browser.sleep(2000);
+        expect(element.all(by.css(".ui-dialog .sample-module")).count()).toBe(1);
 
         var closeBtn = element.all(by.css(".ui-dialog-titlebar button"));
         closeBtn.get(0).click();
-        browser.sleep(300);
+        browser.sleep(2000);
         expect(element.all(by.css(".ui-dialog")).count()).toBe(0);
     });
 
     it('弹出并单击确定', function(){
         var btn = element.all(by.css(".demo button"));
         btn.get(0).click();
-        browser.sleep(300);
+        browser.sleep(2000);
         
         var confirmBtn = element.all(by.css(".ui-dialog-content .sample-module button"));
         confirmBtn.get(0).click();
