7655b645003d3ed3adc0e34b182ec68f51d8acfd
徐荔00111190 <xu.li9@zte.com.cn>
2016-11-23 11:10:26 +0800

    document_2

--- doc/client/demo/common/popupservice/demo4loadModule/template/sample_module.js
+++ doc/client/demo/common/popupservice/demo4loadModule/template/sample_module.js
@@ -4,6 +4,7 @@ define([], function() {
         console.log('SampleModule controller is running..........');
         //只有定义在this上的属性才能发布给外部。
         scope.someData = 'some data defined in the SampleModule controller...';
+        scope.myData = 'sample inter load module manually...';
 
         scope.destroyHandler = function(){
         	PopupService.removePopup(scope.$moduleId);
