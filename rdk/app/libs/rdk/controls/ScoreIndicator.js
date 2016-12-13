a9f9c64e3ebe5ba7c4d35f6b42b7566f0841b321
陈运生6396000843 <chen.yunsheng1@zte.com.cn>
2016-12-13 11:00:16 +0800

    修改得分图控件，支持主题控制，支持显示字体图标

--- rdk/app/libs/rdk/controls/ScoreIndicator.js
+++ rdk/app/libs/rdk/controls/ScoreIndicator.js
@@ -7,22 +7,22 @@ define(['rd.core', 'css!rd.styles.ScoreIndicator',
             return {
                 restrict: 'E',
                 replace: true,
-                template: '<div class="distributionNew">\
-                        <div style="display:flex;justify-content: space-between;">\
-                        <div class="distributionSub">\
-                            <div ng-repeat = "item in config" style="display:flex;">\
-                                <div style="display:flex;align-items: center;margin-left:15px;position:relative">\
-                                    <img alt="img" src="{{item.emotion}}" ng-show="setMark(item)" ng-style="getPosition(item.value)"/>\
-                                    <hr class={{"hrLinePart"+$index}} style="width:6px;border-top-color:{{item.color}};" ng-show="setMark(item)"/>\
-                                    <div style="margin-left: {{item.mark ? 0 : 26}}px;background-color:{{item.color}};width:10px;height:{{setHeight(item.value)}}px;"></div>\
-                                </div>\
-                                <div style="display:flex;">\
-                                    <hr class={{"hrLinePart"+$index}} ng-style="getRealWidth($index)" style="border-top-color:{{item.color}};">\
-                                    <span class="faceSpan" style="color:{{item.color}};">{{item.label + item.value + "%"}}</span>\
+                template: '<div   class="distributionNew">\
+                            <div class="distributionSub">\
+                                <div ng-repeat = "item in config track by $index" style="display:flex;"  class={{"score-base-color"+$index}}>\
+                                    <div class={{"score-bg-color"+$index}} style="position:relative;background-color:{{item.color}};width:10px;height:{{setHeight(item.value)}}px;">\
+                                        <div style="display:flex;position: absolute;align-items: center;top: {{setHeight(item.value)/2-10}}px;right: 10px"  ng-show="setMark(item)">\
+                                            <i ng-if="isIcon(item)" ng-class="iconClass($index,item)" style="font-size: 22px;color:{{item.color}}" />\
+                                            <img ng-if="!isIcon(item)" ng-src="{{item.emotion}}" alt="img"/>\
+                                            <hr style="width:6px;border-top:1px solid {{item.color}};"/>\
+                                        </div>\
+                                        <div style="display:flex;position: absolute">\
+                                            <hr class="hrLinePart" style="border-top-color:{{item.color}}" ng-style="getRealWidth($index,item)">\
+                                            <span id="scoreText" style="white-space: nowrap;color:{{item.color}};">{{item.label + item.value + "%"}}</span>\
+                                        </div>\
+                                    </div>\
                                 </div>\
                             </div>\
-                        </div>\
-                        </div>\
                        </div>',
                 scope: {
                     config: '=?'
@@ -34,11 +34,35 @@ define(['rd.core', 'css!rd.styles.ScoreIndicator',
                 var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
                 scope.initWidth = 20; //初始横线长度
                 scope.widthArr = [];
+                scope.iconClass=function(index,item){
+                    var faSmile="";
+                    if(item.emotion===undefined ||item.emotion===null)
+                    {
+                        switch (index){ //todo:等字体图标实现后再修改
+                            case 0:faSmile="fa-smile-o";break;
+                            case 1:faSmile="fa-smile-o";break;
+                            case 2:faSmile="fa-smile-o";break;
+                            case 3:faSmile="fa-smile-o";break;
+                            default:faSmile="";
+                        }
+                    }else{
+                        faSmile=item.emotion;
+                    }
+                    return "fa "+ faSmile +" faceSpan"+index
+                };
+                scope.isIcon=function(item){
+                    if(item.emotion && item.emotion.match(/[^\s]+\.(jpg|gif|png|bmp)/i))
+                    {
+                        return false;
+                    }
+                    return true;
+                };
+
                 var height = iEle[0].offsetHeight > 0 ? iEle[0].offsetHeight : 300;
 
                 scope.setHeight = function (proValue) {
-                    var parentHeight = height;
-                    var itemHeight = parentHeight * proValue / 100;
+                    var parentHeight = height;//parseInt
+                    var itemHeight =  parentHeight / 100 * proValue ;
                     return itemHeight;
                 };
 
@@ -46,28 +70,20 @@ define(['rd.core', 'css!rd.styles.ScoreIndicator',
                     return item.mark ? true : false;
                 };
 
-                scope.getRealWidth = function (elemID) {
+                scope.getRealWidth = function (elemID,item) {
                     var tmpWidth = scope.initWidth;
                     var tmpWidthArr = scope.widthArr.slice(elemID + 1);
                     angular.forEach(tmpWidthArr, function (data) {
                         tmpWidth = tmpWidth + data + 5;
                     });
                     return {
-                        "width": tmpWidth + "px"
+                        "width": tmpWidth + "px",
                     };
-                };
-
-                scope.getPosition = function (proValue) {
-                    var parentHeight = height;
-                    var itemHeight = parentHeight * proValue / 100;
-                    return {
-                        "top": itemHeight / 2 - 10 + "px"
-                    }
                 };
 
                 if (!!MutationObserver) {
                     var $observer = new MutationObserver(function () {
-                        var labelEleObj = iEle[0].querySelectorAll(".faceSpan");
+                            var labelEleObj = iEle[0].querySelectorAll("#scoreText");
                             scope.$apply(function () {
                                 angular.forEach(labelEleObj, function (data,index) {
                                     if (scope.widthArr.length < index + 1) {
