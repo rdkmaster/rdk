719a2240e1849cb3d3e2ad818df8c8c0630ae825
方亮6176000041 <fang.liang1@zte.com.cn>
2016-12-15 14:41:43 +0800

    btnBug

--- rdk/app/libs/rdk/controls/Button.js
+++ rdk/app/libs/rdk/controls/Button.js
@@ -1,8 +1,8 @@
 define(['rd.core', 'css!rd.styles.Button','css!rd.styles.FontAwesome'
 ], function() {
     var btnSearchApp = angular.module('rd.controls.Button', ['rd.core']);
-    btnSearchApp.directive('rdkButton', ['EventService', 'Utils', 'EventTypes', '$compile', '$timeout', 
-        function(EventService, Utils, EventTypes, $compile, $timeout) {
+    btnSearchApp.directive('rdkButton', ['EventService', 'Utils', 'EventTypes', '$compile',
+        function(EventService, Utils, EventTypes,  $compile) {
             return {
                 restrict: 'E',
                 replace: true,
@@ -11,8 +11,7 @@ define(['rd.core', 'css!rd.styles.Button','css!rd.styles.FontAwesome'
                                 <div class="rdk-button-comment" ng-click="setSelected()">\
                                     <div class="rdk-button-shade rdk-button-{{type}}"></div>\
                                     <button class="rdk-button-btn" ng-mouseover="$mouseOver()" ng-mouseout="$mouseOut()"\
-                                    title="{{tooltip}}" ng-class="{\'rdk-button-selected\':toggle?selected:false,\
-                                        \'rdk-button-enabled\':!enabled,\'rdk-shade-opacity\': mouse}" ng-disabled="!enabled">\
+                                    title="{{tooltip}}" ng-class="setBtnClass()" ng-disabled="!enabled">\
                                         <img src="{{icon}}" ng-click="$stopPro($event)" ng-if="iconShow" ng-class="{\'rdk-padding-right\':paddingHide}">\
                                         <i ng-click="$stopPro($event)" ng-class="{\'rdk-padding-right\':paddingHide}" class="{{icon}}" ng-if="!iconShow"></i>{{label}}\
                                     </button>\
@@ -33,20 +32,22 @@ define(['rd.core', 'css!rd.styles.Button','css!rd.styles.FontAwesome'
                     scope.icon = Utils.getValue(scope.icon, attr.icon, false);
                     scope.enabled = Utils.getValue(scope.enabled, attr.enabled ,true);
                     scope.setSelected=function(){
-                        if(!scope.enabled){
-                            return;
+                        if(scope.toggle==true){
+                            scope.selected=!scope.selected
                         }
-                        // $timeout为了兼容IE11
-                        $timeout(function() {
-                            if(scope.toggle==true){
-                                scope.selected=!scope.selected
-                            }
-                            var fn=scope.click(scope);
-                            if (!!fn) {
-                                fn();
-                            }
-                        }, 0);
+                        var fn = scope.click(scope);
+                        if (!!fn) {
+                            fn();
+                        }
                     }
+                    scope.setBtnClass=function(){
+                        return  {
+                            'rdk-button-selected':scope.toggle?scope.selected:false,
+                            'rdk-button-enabled' :!scope.enabled,
+                            'rdk-shade-opacity': scope.mouse
+                        }
+            
+                    }                 
                     scope.$stopPro=function($event){
                         if(!scope.enabled){
                          $event.stopPropagation();
