9ced3b3a690058e5d4a77e25ab8dea6f09822369
陈旭10045812 <chen.xu8@zte.com.cn>
2016-12-19 14:29:10 +0800

    只修改最大内存到8G

--- rdk/proc/bin/run.sh
+++ rdk/proc/bin/run.sh
@@ -7,7 +7,7 @@ if [ "" != "$pid" ]; then
 	exit
 fi
 #jvm_opts="$jvm_opts -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n"
-jvm_opts="-server -Xms4g -Xmx8g -XX:-UseGCOverheadLimit -XX:+PrintGCDetails $jvm_opts"
+jvm_opts="-server -Xms4g -Xmx8g -XX:-UseGCOverheadLimit $jvm_opts"
 
 if [ -d "./jre" ]; then
     JAVA_HOME="./proc/bin/jre"
