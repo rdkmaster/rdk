9533c73eca6824fb96fb870d5b3cc50668002467
陈旭145812 <chen.xu8@zte.com.cn>
2017-01-08 23:22:04 +0800

    改造部分demo

--- doc/client/demo/common/alert/demo4i18n/i18n.js
+++ doc/client/demo/common/alert/demo4i18n/i18n.js
@@ -2,7 +2,7 @@ define(['rd.modules.i18n'],
     function(i18n) {
 
         //默认语言环境，在获取语言服务失败时使用
-        var defaultLocale = 'zh_CN';
+        var defaultLocale = "en_US";
 
         return i18n.init(
 
@@ -14,20 +14,12 @@ define(['rd.modules.i18n'],
 
             {
                 "en_US": {
-                    welcome: 'Your RDK application is ready!',
-                    wait: 'Please wait for a moment...',
-                    lang: 'Current language is {0}',
-
-                    alert_yes: 'YES...',
-                    alert_no: 'NO...',
-                    alert_ok: 'OK...',
-                    alert_cancel: 'CANCEL...',
+                    alert_yes: 'YES',
+                    alert_no: 'NO',
+                    alert_ok: 'OK',
+                    alert_cancel: 'CANCEL',
                 },
                 "zh_CN": {
-                    welcome: '你的 RDK 应用可以正常工作了！',
-                    wait: '正在查询，请稍候...',
-                    lang: '当前的语言环境是 {0}',
-
                     alert_yes: '是',
                     alert_no: '否',
                     alert_ok: '确定',
@@ -37,5 +29,5 @@ define(['rd.modules.i18n'],
 
             //======================= 结束 =======================
 
-            , "zh_CN")
+            , defaultLocale)
     });
