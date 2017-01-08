9528503014c9a240843f7d128fa426447d5ac9ba
陈旭145812 <chen.xu8@zte.com.cn>
2017-01-08 22:35:28 +0800

    graph支持新版livedemo

---
+++ doc/client/demo/controls/graph/demo.js
@@ -0,0 +1,21 @@
+(function() {
+    // 这些变量和函数的说明，请参考 rdk/app/example/web/scripts/main.js 的注释
+    var downloadDependency = [
+    ];
+    var requiredComponents = [ ], ctx = {};
+    var controllerDefination = ['$scope', 'DataSourceService', 'EventService', main];
+    function main(scope, DataSourceService, EventService) {
+$code
+    }
+
+    var controllerName = 'DemoController';
+    //==========================================================================
+    //                 从这里开始的代码、注释请不要随意修改
+    //==========================================================================
+    define(/*fix-from*/application.getDownloads(downloadDependency)/*fix-to*/, start);
+    function start() {
+        application.initContext(ctx, arguments, downloadDependency);
+        rdk.$injectDependency(application.getComponents(requiredComponents, downloadDependency));
+        rdk.$ngModule.controller(controllerName, controllerDefination);
+    }
+})();
