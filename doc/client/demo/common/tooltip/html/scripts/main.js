ae8e41c48e9e35e465bd4362812a6dcddf738769
徐荔 <xu.li9@zte.com.cn>
2017-01-19 16:16:47 +0800

    富文本

--- doc/client/demo/common/tooltip/html/scripts/main.js
+++ doc/client/demo/common/tooltip/html/scripts/main.js
@@ -1,12 +1,19 @@
-define('main', ['rd.attributes.Tooltip'], function () {
+define('main', ['rd.attributes.Tooltip', 'rd.controls.Button'], function () {
 // 创建一个RDK的应用
-    var app = angular.module("rdk_app", ['rd.attributes.Tooltip']);
+    var app = angular.module("rdk_app", ['rd.attributes.Tooltip', 'rd.controls.Button']);
 // 创建一个控制器
     app.controller('myCtrl', ['$scope',function (scope) {
         /************************ panel demo test data start ************************/
-        scope.content = '<div><h1>hello, h1!</h1> \
-            <h2> hello, h2</h2>\
-            <h3> hello, h3</h3></div>'
+        scope.content = "<div><h5>嵌入按钮</h5> \
+            <rdk_button click='clickHandler($event, yes)' label='{{yes}}'></rdk_button>\
+            <rdk_button click='clickHandler($event, no)' label='{{no}}'></rdk_button></div>"
+
+        scope.clickHandler = function(event, data){
+        	alert(data);
+        }
+
+        scope.yes = 'yes';
+        scope.no = 'no';
     }]);
 
 });
