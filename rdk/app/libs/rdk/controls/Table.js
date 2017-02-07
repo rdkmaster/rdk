45600567adc1915bda416ff40840fef629d46c84
方亮6176000041 <fang.liang1@zte.com.cn>
2017-02-07 14:31:03 +0800

    加字体图标和统一规范

--- rdk/app/libs/rdk/controls/Table.js
+++ rdk/app/libs/rdk/controls/Table.js
@@ -51,7 +51,7 @@ define(['angular', 'jquery', 'underscore', 'jquery-headfix', 'jquery-gesture',
                 <rdk-paging ng-if="pageVisible && pageCtrl && paging && columnDefs.length!=0 && !noData" data-page-size="pageSize" \
                      data-lang="{{lang}}" data-position="{{position}}">\
                 </rdk-paging>\
-                <div ng-if="showExport" class="table-export"><rdk_button click="touchExport" icon="iconfont iconfont-e811" label="{{exportLabel}}"></rdk_button></div>\
+                <div ng-if="showExport" class="table-export"><rdk_button click="touchExport" icon="iconfont iconfont-e8c9" label="{{exportLabel}}"></rdk_button></div>\
                 <div class="clearfix"></div>\
             </div>'
         );
