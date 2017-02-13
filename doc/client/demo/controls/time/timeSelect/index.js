2e83c30f37766327e317e7642b165932c0758d4d
陈运生6396000843 <chen.yunsheng1@zte.com.cn>
2017-02-13 11:04:00 +0800

    删除测试代码

--- doc/client/demo/controls/time/timeSelect/index.js
+++ doc/client/demo/controls/time/timeSelect/index.js
@@ -22,50 +22,6 @@
             var demoUrl = location.pathname.match(/\/doc\/client\/demo\/(.*)\//)[1];
             document.title = dom.length == 0 ? demoUrl : $(dom[0]).attr('doc_title');
         }
-
-        scope.setting = {
-            value: 'now',
-            granularity: "quarter",
-            startDate:"2016-11-01",
-            selectGranularity: true,
-            granularityItems: [{
-                label: "15分钟",
-                value: "quarter"
-            }, {
-                label: "小时",
-                value: "hour"
-            }, {
-                label: "天",
-                value: "date"
-            }, {
-                label: "月",
-                value: "month"
-            }]
-        }
-        scope.test = {
-            value: 'now',
-            granularity: "quarter",
-            startDate:"2016-11-01",
-            selectGranularity: true,
-            granularityItems: [{
-                label: "15分钟",
-                value: "quarter"
-            }, {
-                label: "小时",
-                value: "hour"
-            }, {
-                label: "天",
-                value: "date"
-            }, {
-                label: "月",
-                value: "month"
-            }]
-        }
-
-        scope.clkData=function(){
-            scope.test.value="2017-2-09";
-            scope.setting.value="2016-2-9";
-        }
     };
 
     require.config({paths:{helper: '/doc/tools/doc_js/doc_app_helper'}});
