d86ceaf58717467d8eaa00c5d499d9787afbb3d5
方亮6176000041 <fang.liang1@zte.com.cn>
2017-04-06 14:49:36 +0800

    prompt box

---
+++ doc/client/demo/comprehensive/panel/promptBox/demo.js
@@ -0,0 +1,30 @@
+(function() {
+    // 这些变量和函数的说明，请参考 rdk/app/example/web/scripts/main.js 的注释
+    var imports = [
+        'rd.controls.Module', 'rd.services.PopupService', 'base/template/prompt_module','bootstrap-select',
+        'css!base/css/prompt_module','rd.controls.Button'
+    ];
+    var extraModules = [ ];
+    var controllerDefination = ['$scope', 'PopupService', main];
+    function main(scope, PopupService) {
+        var moduleID;
+        scope.load = function(){
+            var sampleUrl = 'template/prompt_module.html';
+            var initData = {};
+            var myOption = {
+            }
+            moduleID = PopupService.popup(sampleUrl, initData, myOption);
+        }
+    }
+
+    var controllerName = 'DemoController';
+    //==========================================================================
+    //                 从这里开始的代码、注释请不要随意修改
+    //==========================================================================
+    define(/*fix-from*/application.import(imports)/*fix-to*/, start);
+    function start() {
+        application.initImports(imports, arguments);
+        rdk.$injectDependency(application.getComponents(extraModules, imports));
+        rdk.$ngModule.controller(controllerName, controllerDefination);
+    }
+})();
