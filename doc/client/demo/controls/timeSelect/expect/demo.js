fb97a2378ebc179074cd85204671ae25863100e2
陈运生6396000843 <chen.yunsheng1@zte.com.cn>
2017-04-28 15:46:29 +0800

    timeSelect

---
+++ doc/client/demo/controls/timeSelect/expect/demo.js
@@ -0,0 +1,47 @@
+(function() {
+    // 这些变量和函数的说明，请参考 rdk/app/example/web/scripts/main.js 的注释
+    var imports = [
+        'rd.controls.ComboSelect','rd.controls.TimeSelect','css!base/css/time-select'
+    ];
+    var extraModules = [ ];
+    var controllerDefination = ['$scope', 'EventService','EventTypes', main];
+    function main(scope,EventService,EventTypes ) {
+        //时间配置
+        scope.granularitySet={
+            value: '2017-02-1',
+            granularity: "date",
+            selectGranularity:true,
+            startDate:"2016-01-1 13:30", //可选的开始时间，类型字符串/Date对象
+            //endDate:"now+20d",  //可选的结束时间，类型字符串/Date对象
+            expectSelectedDate:[
+                {
+                    year:'2016',
+                    expectStartDate:{month:'3','day':"5"},
+                    expectEndDate:{month:'3','day':"27"}
+                },
+                {
+                    year:'2017',
+                    expectStartDate:{month:'2','day':"14"},
+                    expectEndDate:{month:'3','day':"4"}
+                }
+            ]
+        };
+
+        EventService.register('timeID', EventTypes.CHANGE, function(event, data){
+            scope.comboOpenStatus=false;
+            EventService.broadcast('comboID', EventTypes.CHANGE, data);
+        });
+
+    }
+
+    var controllerName = 'DemoController';
+    //==========================================================================
+    //                 从这里开始的代码、注释请不要随意修改
+    //==========================================================================
+    define(/*fix-from*/application.import(imports)/*fix-to*/, start);
+    function start() {
+        application.initImports(imports, arguments);
+        rdk.$injectDependency(application.getComponents(extraModules, imports));
+        rdk.$ngModule.controller(controllerName, controllerDefination);
+    }
+})();
--- rdk/app/libs/rdk/controls/TimeSelect.js
+++ rdk/app/libs/rdk/controls/TimeSelect.js
@@ -20,9 +20,14 @@ define(['rd.core','rd.controls.TimeBasic','css!rd.styles.TimeSelect'],
                                     {{granularityItem.label}}\
                                     </span>\
                                 </div>\
-                            <div class="rdk-time-select-module">\
-                               <input ng-show="false" ng-model="setting.value"  type="text">\
-                           </div></div>',
+                                <div class="rdk-time-select-module">\
+                                    <input ng-show="false" ng-model="setting.value"  type="text">\
+                                </div>\
+                                <div ng-if="hasExpect" class="color-tips">\
+                                    <span>Selected</span>\
+                                    <span>Recommended</span>\
+                                </div>\
+                            </div>',
                 scope:scopeDefine,
                 link:function(scope, iElement, iAttrs) {
                     var initValue = [];
@@ -34,7 +39,11 @@ define(['rd.core','rd.controls.TimeBasic','css!rd.styles.TimeSelect'],
                         {label: "Week", value: "week"},
                         {label: "Month", value: "month"}
                     ];
+                    var charToNum = {一:1, 二:2, 三:3, 四:4, 五:5, 六:6, 七:7, 八:8, 九:9, 十:10, 十一:11, 十二:12};
+
                     scope.language = Utils.getLocale(scope);
+
+                    scope.hasExpect=false;
 
                     scope.activeGranularityCls = function(granularity){
                         return scope.setting.granularity == granularity;
@@ -113,7 +122,7 @@ define(['rd.core','rd.controls.TimeBasic','css!rd.styles.TimeSelect'],
                         if (angular.isUndefined(scope.setting)) {
                             scope.setting = {};
                         }
-                        scope.setting.weekValue = null;
+                        scope.setting.weekValue =  null;
                         scope.setting.selectGranularity = Utils.getValue(scope.setting.selectGranularity, undefined, false);
                         scope.setting.selectGranularity = scope.setting.selectGranularity && (angular.isArray(scope.setting.selectGranularity) ? scope.setting.selectGranularity : defaultGranularity);
                         scope.setting.granularity = Utils.getValue(scope.setting.granularity, undefined, TimeUnit.DAY);
@@ -130,6 +139,7 @@ define(['rd.core','rd.controls.TimeBasic','css!rd.styles.TimeSelect'],
                         } else {
                             scope.setting.startDate = Utils.getValue(TimeUtilService.dateFormate(TimeUtilService.getTimeMacroCalculate(scope.setting.startDate), scope.timeFormat), undefined, null);
                         }
+                        debugger;
                         if (angular.isUndefined(scope.setting.endDate) || !scope.setting.endDate) {
                             scope.setting.endDate = Utils.getValue(TimeUtilService.getTimeMacroCalculate(scope.setting.endDate), undefined, null);
                         } else {
@@ -159,7 +169,184 @@ define(['rd.core','rd.controls.TimeBasic','css!rd.styles.TimeSelect'],
                         scope.condition.startTime = scope.setting.value;
                         _datetimepicker(iElement[0].querySelector(".rdk-time-select-module"),scope.startTimeOption);
 
+                        _setExpectDate();
+                        _setWeekStyle();
                     }
+
+                    function _setExpectDate(){
+                        console.log("_setExpectDate");
+                        debugger;
+                        if(!(scope.setting && scope.setting.expectSelectedDate)){
+                            return
+                        }
+
+                        var  vaildExpectDate =  typeof vaildExpectDate=='undefined'?_vaildExpectDate():vaildExpectDate;
+                        if(!vaildExpectDate){
+                            console.error("setting expectSelectedDate is error");
+                            return
+                        }
+                        scope.hasExpect=true;
+                        _expectDateHandler();
+                        _bindEventHandler();
+                    }
+
+                    function _expectDateHandler(){
+
+                        var yearsNode=_getDatePickerNode("years");
+
+                        _searchDateForYear(scope.setting.expectSelectedDate,yearsNode);
+
+                        var monthsNode=_getDatePickerNode("months");
+                        var monthsHeadNode=_getDatePickerNode("months",true);
+
+                        _searchDateForMonth(scope.setting.expectSelectedDate,monthsNode,monthsHeadNode);
+
+                        var daysNode=_getDatePickerNode("days");
+                        var daysHeadNode=_getDatePickerNode("days",true);
+
+                        var daysObj=parseDay(daysHeadNode.innerText);
+                        _searchDateForDay(scope.setting.expectSelectedDate,daysNode,daysObj);
+                    }
+
+                    function _bindEventHandler(){
+                        var datetimepickerDom=iElement[0].querySelector(".rdk-time-select-module .datetimepicker");
+                        $(datetimepickerDom).on("click",function(){
+                            setTimeout(_expectDateHandler,0);
+                        });
+                    }
+
+                    function _getDatePickerNode(granularity,headVal){
+                        var selectorBefore=".rdk-time-select-module .datetimepicker .datetimepicker-";
+                        var selectorAfter=">table>tbody>tr>";
+                        var nodeName="";
+                        if (granularity=="years" || granularity=="months"){
+                            nodeName="td>span";
+                        }else{
+                            nodeName="td";
+                        }
+                        var selector=selectorBefore+granularity+selectorAfter+nodeName;
+                        if(!!headVal){
+                            var theadNode=">table>thead>tr>th.switch[colspan]";
+                            selector=selectorBefore+granularity+theadNode;
+                            return iElement[0].querySelector(selector);
+                        }
+                        return iElement[0].querySelectorAll(selector);
+                    }
+                    function _searchDateForYear(targetArr,DateArr){
+                        for (var i= 0,iLen=targetArr.length ; i<iLen ; i++)
+                        {
+                            for (var j= 0, jLen=DateArr.length; j<jLen; j++)
+                            {
+                                if(targetArr[i].year==DateArr[j].innerText){
+                                    if(!DateArr[j].classList.contains("disabled")){
+                                        DateArr[j].classList.add("expect-day");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    function _searchDateForMonth(targetArr,DateArr,headVal){
+                        for (var i= 0,iLen=targetArr.length ; i<iLen ; i++)
+                        {
+                            if(targetArr[i].year==headVal.innerText){
+                                for (var j= 0, jLen=DateArr.length; j<jLen; j++)
+                                {
+                                    if(targetArr[i].expectStartDate.month==parseMonth(DateArr[j].innerText)
+                                        || targetArr[i].expectEndDate.month==parseMonth(DateArr[j].innerText))
+                                    {
+                                        if(!DateArr[j].classList.contains("disabled"))
+                                        {
+                                            DateArr[j].classList.add("expect-day");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    function _searchDateForDay(targetArr,DateArr,headVal){
+                        for (var i= 0,iLen=targetArr.length ; i<iLen ; i++)
+                        {
+                            if(targetArr[i].year==headVal.year &&
+                                (targetArr[i].expectStartDate.month==headVal.month || targetArr[i].expectEndDate.month==headVal.month)){
+                                for (var j= 0, jLen=DateArr.length; j<jLen; j++)
+                                {
+                                    if(+targetArr[i].expectStartDate.month!=+targetArr[i].expectEndDate.month){
+                                        if(!DateArr[j].classList.contains("old") && !DateArr[j].classList.contains("new")){
+                                            if((targetArr[i].expectStartDate.month==headVal.month && +DateArr[j].innerText>=+targetArr[i].expectStartDate.day)
+                                                || targetArr[i].expectEndDate.month==headVal.month && +DateArr[j].innerText<=+targetArr[i].expectEndDate.day)
+                                            {
+                                                DateArr[j].classList.add("expect-day");
+                                            }
+                                            if(+DateArr[j].innerText == targetArr[i].expectStartDate.day){
+                                                DateArr[j].classList.add("border-left");
+                                            }
+                                        }
+                                        else  if(DateArr[j].classList.contains("new")){
+                                            if(targetArr[i].expectStartDate.month==headVal.month && +DateArr[j].innerText<=+targetArr[i].expectEndDate.day)
+                                            {
+                                                DateArr[j].classList.add("expect-day");
+                                            }
+                                            if(+DateArr[j].innerText == targetArr[i].expectEndDate.day){
+                                                DateArr[j].classList.add("border-right");
+                                            }
+                                        }else if(DateArr[j].classList.contains("old")){
+                                            if(targetArr[i].expectEndDate.month==headVal.month && +DateArr[j].innerText>=+targetArr[i].expectStartDate.day){
+                                                DateArr[j].classList.add("expect-day");
+                                            }
+                                        }
+                                    }else{
+                                        if(!DateArr[j].classList.contains("old") && !DateArr[j].classList.contains("new")){
+                                            if(+DateArr[j].innerText >= +targetArr[i].expectStartDate.day && +DateArr[j].innerText <= +targetArr[i].expectEndDate.day){
+                                                DateArr[j].classList.add("expect-day");
+                                            }
+                                            if(+DateArr[j].innerText == targetArr[i].expectStartDate.day){
+                                                DateArr[j].classList.add("border-left");
+                                            }
+                                            else if(+DateArr[j].innerText == targetArr[i].expectEndDate.day){
+                                                DateArr[j].classList.add("border-right");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    function _vaildExpectDate(){
+
+                        var expectSelectedDate = scope.setting.expectSelectedDate;
+                        if(expectSelectedDate.length==0){
+                            return false
+                        }else{
+                            for(var i= 0,len=expectSelectedDate.length;i<len;i++){
+                                if(!expectSelectedDate[i].hasOwnProperty("year")){
+                                    return false
+                                }
+                                if(!(expectSelectedDate[i].hasOwnProperty("expectStartDate")
+                                    && expectSelectedDate[i].hasOwnProperty("expectEndDate"))){
+                                    return false
+                                }else{
+                                    if(!(expectSelectedDate[i].expectStartDate.hasOwnProperty("month")
+                                        && expectSelectedDate[i].expectStartDate.hasOwnProperty("day"))){
+                                        return false
+                                    }
+                                }
+                            }
+                        }
+                        return true
+                    }
+
+                    function parseMonth(val){
+                        return charToNum[val.replace("月",'')];
+                    }
+                    function parseDay(val){
+                        var result={};
+                        val = val.split("月");
+                        result.month=charToNum[val[0].trim()];
+                        result.year=val[1].trim();
+                        return result;
+                    }
+
                     function handleWeekValue() {
                         if (scope.selectedGranularity.value != "week"){
                             return;
@@ -222,11 +409,24 @@ define(['rd.core','rd.controls.TimeBasic','css!rd.styles.TimeSelect'],
                         }
                         return option;
                     }
+                    function _setWeekStyle(){
+                        if (scope.selectedGranularity.value == "week") {
+                            handleWeekValue();
+                            setTimeout(function () {
+                                var trDom = iElement[0].querySelector(".datetimepicker .datetimepicker-days>table>tbody>tr>td.active").parentNode;
+                                trDom.classList.add("active");
+                            }, 0)
+                        }
+                    }
                     function _datetimepicker(domNode,option){
                         !!datetimepicker && datetimepicker.datetimepicker('remove');
                         datetimepicker = $(domNode).datetimepicker(option);
                         datetimepicker.datetimepicker('update');//增加个隐藏的input节点目的:解决没法更新时间的BUG
                         datetimepicker.on('changeDate', function(ev) {
+
+                            _setExpectDate();
+                            _setWeekStyle();
+
                             scope.$apply(function() {
                                 scope.setting.value = TimeUtilService.dateFormate(new Date(ev.date.valueOf()), scope.startTimeOption.format);
                                 EventService.raiseControlEvent(scope, EventTypes.CHANGE, scope.setting.weekValue || scope.setting.value);
