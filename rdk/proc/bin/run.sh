ed69955f655a66369c38abc5804b49b36b4b9a52
陈旭10045812 <chen.xu8@zte.com.cn>
2017-04-28 14:10:51 +0800

    如果errors.log不存在则创建一个

--- rdk/proc/bin/run.sh
+++ rdk/proc/bin/run.sh
@@ -23,4 +23,7 @@ program_root=`cd "$DIRNAME"/; pwd`
 classPath="$program_root/proc/bin/lib/*"
 
 echo "RDK Server is starting..."
+if [ ! -e proc/logs/errors.log ]; then
+	touch proc/logs/errors.log
+fi
 $JAVA_HOME/bin/java $jvm_opts -Dfile.encoding=UTF-8 -D$rdk_flag -classpath "$classPath" com.zte.vmax.rdk.Run 2>> proc/logs/errors.log &
