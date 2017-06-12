946da905718bb7c29897a0367c6937f0d0b67267
方亮6176000041 <fang.liang1@zte.com.cn>
2017-06-12 15:47:21 +0800

    单个列过滤

--- doc/client/demo/comprehensive/select/multi-menu/demo.js
+++ doc/client/demo/comprehensive/select/multi-menu/demo.js
@@ -1,7 +1,7 @@
 (function() {
     // 这些变量和函数的说明，请参考 rdk/app/example/web/scripts/main.js 的注释
     var imports = [
-        'rd.controls.ComboSelect', 'rd.controls.BasicSelector',
+        'rd.controls.ComboSelect', 'rd.controls.BasicSelector','css!rd.styles.IconFonts',
         'rd.containers.Accordion','rd.attributes.Scroll', 'css!base/css/menu'
     ];
     var extraModules = [ ];
--- doc/client/demo/controls/table/customheader/demo.js
+++ doc/client/demo/controls/table/customheader/demo.js
@@ -1,25 +1,39 @@
 (function() {
     // 这些变量和函数的说明，请参考 rdk/app/example/web/scripts/main.js 的注释
     var imports = [
-        'rd.controls.Table', 'rd.services.Alert'
+        'rd.controls.Table', 'rd.services.Alert', 'rd.controls.ComboSelect', 'rd.controls.BasicSelector',
+        'css!base/custom'
     ];
     var extraModules = [ ];
-    var controllerDefination = ['$scope', 'Alert', main];
-    function main($scope, Alert) {
+    var controllerDefination = ['$scope', 'Alert','DataSourceService','$timeout', main];
+    function main($scope, Alert, DataSourceService, $timeout) {
+        var arr=[],arrData=[],numTarget;
+        $scope.sizeColor = true;
+        $timeout(function(){
+            console.log($scope.ds_table.header)
+        },0)
         $scope.setting = {
             "columnDefs" :[
                 {
-                    targets : 0,
-                    sortable: true
-                },
-                {
                     title : function(data, target) {
-                        return '<span>自定义带select的列</span>\
-                                <select ng-change="titleExtraSelecteHandler(titleExtraSelected)"\
-                                        ng-model="titleExtraSelected"\
-                                        ng-options="optionItem.label as optionItem.label for optionItem in titleOptions">\
-                                    <option value="">-- choose an item --</option>\
-                                </select>'
+                        console.log($scope)
+                        $scope.allItems=[];
+                        var total = []
+                        var j = 0;
+                        for(var i=0;i< arrData.length;i++){
+                            if(total.indexOf(arrData[i][target]) === -1){
+                                total.push(arrData[i][target]);
+                                $scope.allItems[j]={};
+                                $scope.allItems[j].id=j;
+                                $scope.allItems[j].label= arrData[i][target]
+                                j++;
+                            }
+                        }
+                        return '<div style="position: absolute;top:6px;right: 2px;width: 100%;text-align: center;margin-top:0" >你好<i class="iconfont iconfont-e92a pticon" ng-class="{colorGray:sizeColor}" ng-click="selectorShow(1)"></i>\
+                                <div ng-show="selectShow" class="selectorContent">\
+                        <rdk_basic_selector data="allItems" selected_items="allSelected" multiple_select="true" searchable="false" editable="false" change="selectorChanged"><span>{{item.label}}</span>\
+                        </rdk_basic_selector>\
+                        </div></div>'
                     },
                     targets : 1,
                     sortable: true
@@ -45,22 +59,54 @@
                                         ng-model="titleExtraSelected"\
                                         ng-options="item[2] as item[2]  for item in data">\
                                     <option value="">-- choose an item --</option>\
-                                </select>'
+                        </select>'
                     },
                     render : '<a ng-click="appScope.click(item)" href="javascript:void(0)">点击</a>'
                 }
             ]
         };
+        $scope.selectShow = false;
+        $scope.selectorShow = function(target){
+            numTarget = target;
+            $scope.selectShow = !$scope.selectShow;
+            $scope.selectSho = false;
+
+        }
+        function flter(items,item,target){//过滤
+            var objective = [], residue= []
+            for(var j=0;j<item.length;j++){
+                for(var i=0;i<items.length;i++){
+                    if(items[i][target].indexOf(item[j])!==-1){
+                        objective.push(items[i])
+                    }
+                }
+            }
+            return objective
+        }
+        $scope.selectorChanged = function(context,selected ,index) {
+            $scope.sizeColor=!!selected.length ? false:true;
+            arr=[]
+            for(var i = 0;i<selected.length;i++){
+                arr.push(selected[i].label);
+            }
+            var ds = DataSourceService.get('ds_table');
+            var condition = {};
+            ds.query(condition)
+        }
+        $scope.tableProcessor=function(data) {
+            console.log(data)
+            arrData=JSON.parse(JSON.stringify(data.data))
+            if (!!arr.length) {
+                data.data = flter(data.data, arr, numTarget)
+            }
+            return data
+        }
         $scope.click = function(item) {
             //弹出选择栏，重填value
             Alert.scope = $scope;
             Alert.confirm("自定义editorRenderer，弹出演示", "提示框");
         };
         //titleOptions的数据结构根据自身业务逻辑来定
-        $scope.titleOptions = [
-            {label: 'item1', id: 1}, {label: 'item2', id: 2},
-            {label: 'item3', id: 3}, {label: 'item4', id: 4}
-        ];
         //变化后的处理逻辑
         $scope.titleExtraSelecteHandler = function(selected) {
             alert("你选择了"+selected);
