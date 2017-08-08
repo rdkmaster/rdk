e2e7252e2d089f2cd44935996180b8a6f1357643
陈旭10045812 <chen.xu8@zte.com.cn>
2017-08-08 16:16:28 +0800

    rdk自身支持版本控制

--- doc/tools/add-version-info/add-version-info.js
+++ doc/tools/add-version-info/add-version-info.js
@@ -2,6 +2,20 @@
 var fs = require('fs');
 
 var args = normalizeArgs(process.argv.slice(2));
+global.args = args;
+if (args.hasOwnProperty('h')) {
+    printUsageAndExit('');
+}
+if (!args.v || !args.c) {
+    printUsageAndExit('version(-v) and code home(-c) is required!');
+}
+if (!fs.existsSync(args.c)) {
+    printUsageAndExit('code home[' + args.c + '] do not exist!');
+}
+if (!fs.lstatSync(args.c).isDirectory()) {
+    printUsageAndExit('code home[' + args.c + '] need to be a directory!');
+}
+
 console.log('code home:  ' + args.c);
 console.log('version:    ' + args.v);
 console.log('extensions: ' + args.e);
@@ -40,23 +54,23 @@ function normalizeArgs(argv) {
     };
     res.e = res.e ? res.e.split(/\s*,\s*/g) : ['js', 'html', 'css'];
     res.x = res.x ? res.x.split(/\s*,\s*/g) : ['index.html'];
-    if (!res.v || !res.c) {
-        printUsageAndExit('version(-v) and code home(-c) is required!');
+    if (res.c) {
+        res.c = res.c.match(/[\\\/]\s*$/) ? res.c.substring(0, res.c.length-1) : res.c;
     }
-    if (!fs.existsSync(res.c)) {
-        printUsageAndExit('code home[' + res.c + '] do not exist!');
-    }
-    if (!fs.lstatSync(res.c).isDirectory()) {
-        printUsageAndExit('code home[' + res.c + '] need to be a directory!');
-    }
-    res.c = res.c.match(/[\\\/]\s*$/) ? res.c.substring(0, res.c.length-1) : res.c;
     return res;
 }
 
 function printUsageAndExit(msg) {
-    console.log(msg);
-    console.log();
+    if (msg) {
+        console.log(msg);
+        console.log();
+    }
     console.log('Usage:');
-    console.log('  add-version-info -v version -c code-home [-e extensions] [-x excludes]');
+    console.log('  add-version-info -v version -c code-home [-e extensions] [-x excludes] [-h]');
+    console.log('     -v 是版本号，例如1.1.1。注意0.0.0是rdk保留的版本号，请别使用。');
+    console.log('     -c 是待转换源码所在路径');
+    console.log('     -e 是需要处理的文件的扩展名，默认值是 js,html,css。多个扩展名请用英文逗号隔开');
+    console.log('     -x 是不需要处理的文件列表，默认值是 index.html。多个文件请用英文逗号隔开');
+    console.log('     -h 显示这些信息');
     process.exit(1);
 }
---
+++ rdk/app/libs/rdk/add-version-info.js
@@ -0,0 +1,12 @@
+//先执行一下公共的add-version-info
+require(`${__dirname}/../../../../doc/tools/add-version-info/add-version-info.js`);
+
+var fs = require('fs');
+
+var content = fs.readFileSync(`${__dirname}/rdk.js`).toString();
+fs.wirteFileSync(`${__dirname}/rdk.js`, content.replace("mainconfig", "mainconfig-" + args.v));
+
+fs.rename(`${__dirname}/mainconfig.js`, `${__dirname}/mainconfig-${args.v}.js`);
+content = fs.readFileSync(`${__dirname}/mainconfig-${args.v}.js`).toString();
+fs.wirteFileSync(`${__dirname}/mainconfig-${args.v}.js`,
+    content.replace(/"(rd\.(attributes|containers|controls|services)\.\w+?)":\s*"(.+?)"\s*,/g, `"$1": "$3-${args.v}"`));
